3f4b69ba78976964b82f2726155c96170229512d
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-09-13 09:31:03 +0900

    change fetch from jQuery ajax to vanila fetch

--- week5/static/js/main.js
+++ week5/static/js/main.js
@@ -3,14 +3,51 @@ $(document).ready(function () {
 });
 
 const showStar = async () => {
+  const cardContainer = document.getElementById("star-box");
   const response = await fetch("/api/list", {
-    method: "POST",
+    method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
   });
+  if (response.status !== 200) console.log("GET request error!");
+  const result = await response.json();
+  const stars = result.stars;
+  let html = "";
 
-  console.log(response);
+  stars.forEach((star) => {
+    html += `<div class="card">
+                      <div class="card-content">
+                        <div class="media">
+                          <div class="media-left">
+                            <figure class="image is-48x48">
+                              <img
+                                src=${star.img_url} />
+                            </figure>
+                          </div>
+                          <div class="media-content">
+                            <a href="#" target="_blank" class="star-name title is-4">${star.name} (좋아요: ${star.like})</a>
+                            <p class="subtitle is-6">${star.recent}</p>
+                          </div>
+                        </div>
+                      </div>
+                      <footer class="card-footer">
+                        <a href="#" onclick="likeStar('${star.name}')" class="card-footer-item has-text-info">
+                          위로!
+                          <span class="icon">
+                            <i class="fas fa-thumbs-up"></i>
+                          </span>
+                        </a>
+                        <a href="#" onclick="deleteStar('${star.name}')" class="card-footer-item has-text-danger">
+                          삭제
+                          <span class="icon">
+                            <i class="fas fa-ban"></i>
+                          </span>
+                        </a>
+                      </footer>
+                    </div>`;
+  });
+  cardContainer.innerHTML = html;
 };
 
 // showStar();
