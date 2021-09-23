5de98dd1fdef20d23ee7a8ba487b3f2bb1b05620
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-09-23 14:15:39 +0900

    Node.js, express, mongoose recap

--- week7/express/index.js
+++ week7/express/index.js
@@ -1,9 +1,50 @@
+const mongoose = require("mongoose");
+const { render } = require("ejs");
 const express = require("express");
 const app = express();
 const port = 3000;
 
+const connect = require("./schemas");
+connect();
+const goodsRouter = require("./routes/goods");
+const userRouter = require("./routes/user");
+
+// let express uderstands and transforms the form values into javascript
+app.use(express.urlencoded({ extended: true }));
+// let express to understands the text data
+app.use(express.json());
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+// use virtual file system in browser
+// app.use("/uploads", express.static("uploads"));
+app.use("/", express.static("public"));
+
+app.use((req, res, next) => {
+  next();
+});
+
+app.use("/goods", goodsRouter);
+app.use("/user", userRouter);
+
+app.get("/test", (req, res) => {
+  let name = req.query.name;
+  console.log(req.query);
+  res.render("test", { name });
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/home", (req, res) => {
+  res.render("index");
+});
+
+app.get("/detail", (req, res) => {
+  console.log(req.query);
+  res.render("detail");
 });
 
 app.listen(port, () => {
